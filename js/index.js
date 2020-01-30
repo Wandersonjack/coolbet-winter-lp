e3536db345090fc7a1a613bbd601b7b34df94cfb
wandersonjack <wander8jackson@gmail.com>
2020-01-30 21:13:19 +0200

    motion

---
+++ js/index.js
@@ -0,0 +1,14 @@
+AOS.init();
+
+function openEvent() {
+  const hamburger = document.querySelector('.hamburger');
+  const navLinks = document.querySelector('.nav-links');
+  const links = document.querySelectorAll('.nav-links li');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+      link.classList.toggle('fade');
+    });
+  });
+}
